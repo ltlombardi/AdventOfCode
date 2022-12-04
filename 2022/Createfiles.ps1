cb75213026fe0a37c1c80ea553265afe405f41bc
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-12-04 18:19:39 -0300

    Script to create all the daily files

---
+++ 2022/Createfiles.ps1
@@ -0,0 +1,33 @@
+function FunctionName {
+    param (
+        $name,
+        $day
+    )    
+    return @"
+    class $name
+    {
+        internal static async Task<int> Solution()
+        {
+            var lines = await File.ReadAllLinesAsync("InputDay$day.txt");
+            var result = 0;
+            return result;
+        }
+    }
+"@
+}
+
+for ($i = 1; $i -le 25; $i++) {
+    $name = "Day${i}Part1" 
+    $file = New-Item "$name.cs"
+    $class = FunctionName $name  $i
+    Set-Content $file -Value   $class 
+
+    $name = "Day${i}Part2" 
+    $file = New-Item "$name.cs"
+    $class = FunctionName $name $i
+    Set-Content $file -Value $class
+
+    $name = "Day${i}Input" 
+    $file = New-Item "$name.txt"
+}
+
