9b74fb2acb5cac8982bb86978fe47653a0ef3bd1
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-12-04 18:19:38 -0300

    Small performance gain

--- 2017/AdventOfCode/Program.cs
+++ 2017/AdventOfCode/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
             var daysToExecute = new int[] { 10, 14 };
             var IDoableClasses = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
                 .Where(t => t.GetInterfaces().Contains(typeof(IDoable))).Select(t => Activator.CreateInstance(t)).Cast<IDoable>();
-            var daysToExecuteString = daysToExecute.Select(d => "Day" + d);
+            var daysToExecuteString = daysToExecute.Select(d => "Day" + d).ToArray();
             foreach (var item in IDoableClasses) { if (daysToExecuteString.Contains(item.GetType().Name)) item.Do(); }
 
             Console.ReadLine();
