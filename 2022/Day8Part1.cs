059c5cd5ce8b460cbb1a92d389e13e6e15d5ae1f
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-12-08 10:44:51 -0300

    wip

--- 2022/Day8Part1.cs
+++ 2022/Day8Part1.cs
@@ -1,9 +1,30 @@
-    class Day8Part1
+class Day8Part1
+{
+    internal static async Task<string> Solution()
     {
-        internal static async Task<string> Solution()
+        var lines = await File.ReadAllLinesAsync("Day8Input.txt");
+        var result = 0;
+        for (int i = 0; i < lines.Length; i++)
         {
-            var lines = await File.ReadAllLinesAsync("Day8Input.txt");
-            var result = 0;
-            return result.ToString();
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                bool isEdge = i == 0 || i == lines.Length - 1 || j == 0 || j == lines[i].Length - 1;
+                if (isEdge || IsInteriorVisible(lines, i, j))
+                {
+                    result++;
+                }
+            }
         }
+
+        return result.ToString();
     }
+
+    private static bool IsInteriorVisible(string[] lines, int i, int j)
+    {
+        
+        return Char.GetNumericValue(lines[i][j]) > Char.GetNumericValue(lines[i][j + 1])
+        || Char.GetNumericValue(lines[i][j]) > Char.GetNumericValue(lines[i][j - 1])
+        || Char.GetNumericValue(lines[i][j]) > Char.GetNumericValue(lines[i - 1][j])
+        || Char.GetNumericValue(lines[i][j]) > Char.GetNumericValue(lines[i + 1][j]);
+    }
+}
--- 2022/Program.cs
+++ 2022/Program.cs
@@ -12,6 +12,7 @@ var solutions = new[,]
     { Day5Part1.Solution, Day5Part2.Solution},
     { Day6Part1.Solution, Day6Part2.Solution},
     { Day7Part1.Solution, Day7Part2.Solution},
+    { Day8Part1.Solution, Day8Part2.Solution},
 };
 var day = dayToExecute ?? solutions.Length / 2;
 var result = await solutions[day - 1, 0].Invoke();
