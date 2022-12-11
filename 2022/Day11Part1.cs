6a1a6e57a2c456847a16184fdd8d8335caae520f
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-12-11 16:14:31 -0300

    Improvements

--- 2022/Day11Part1.cs
+++ 2022/Day11Part1.cs
@@ -9,11 +9,11 @@ class Day11Part1
         {
             var items = lines[i + 1].Trim().Split(':')[1].Split(',').Select(int.Parse);
             var operation = lines[i + 2].Trim().Split(' ');
-            int operand = operation[5] == "old" ? 0 : int.Parse(operation[5]);
+            var operand = operation[5] == "old" ? 0 : int.Parse(operation[5]);
             var @operator = operation[5] == "old" ? "^" : operation[4];
-            int divisibleBy = int.Parse(lines[i + 3].Trim().Split(' ')[3]);
-            int testPassMonkey = int.Parse(lines[i + 4].Trim().Split(' ')[5]);
-            int testFailMonkey = int.Parse(lines[i + 5].Trim().Split(' ')[5]);
+            var divisibleBy = int.Parse(lines[i + 3].Trim().Split(' ')[3]);
+            var testPassMonkey = int.Parse(lines[i + 4].Trim().Split(' ')[5]);
+            var testFailMonkey = int.Parse(lines[i + 5].Trim().Split(' ')[5]);
             monkeys.Add(new Monkey(
                 items,
                 @operator,
@@ -23,27 +23,29 @@ class Day11Part1
                 testFailMonkey
                 ));
         }
-        foreach (var _ in Enumerable.Range(0, 20))
+
+        const int Rounds = 20;
+        foreach (var _ in Enumerable.Range(0, Rounds))
         {
             foreach (var monkey in monkeys)
             {
                 monkey.Act(monkeys);
             }
         }
-        var temp = monkeys.Select(m => m.InspectCount).OrderDescending().ToList();
-        result = temp.Take(2).Aggregate((a, b) => a * b);
+        var inspections = monkeys.Select(m => m.InspectCount).ToList();
+        result = inspections.OrderDescending().Take(2).Aggregate((a, b) => a * b);
         return result.ToString();
     }
 
     class Monkey
-    { // This is an experiment in omitting accessors and using defaults
+    { // This is an experiment in omitting accessors and using defaults accessors
         public Queue<int> ItemWorries { get; }
+        public int InspectCount { get; private set; }
         int DivisibleBy { get; }
         int TestPassMonkey { get; }
         int TestFailMonkey { get; }
         string Operator { get; }
         int Operand { get; }
-        public int InspectCount { get; private set; }
 
         public Monkey(
             IEnumerable<int> worry,
@@ -67,7 +69,7 @@ class Day11Part1
             {
                 var item = ItemWorries.Dequeue();
                 var worry = Relief(Inspect(item));
-                monkeys[Throw(worry)].ItemWorries.Enqueue(worry);
+                monkeys[ThrowToWhichMonkey(worry)].ItemWorries.Enqueue(worry);
             }
         }
         int Relief(int worry) => worry / 3;
@@ -81,8 +83,7 @@ class Day11Part1
                 default: return worry + Operand;
             }
         }
-        int Throw(int worry) => Test(worry) ? TestPassMonkey : TestFailMonkey;
-        bool Test(int worry) => worry % DivisibleBy == 0;
+        int ThrowToWhichMonkey(int worry) => worry % DivisibleBy == 0 ? TestPassMonkey : TestFailMonkey;
     }
 }
 
