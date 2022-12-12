b93bb04d03c04e90715042e8a483ba09ad522fad
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-12-12 11:44:18 -0300

    Wip with stackoverflow exception

--- 2022/Day12Part1.cs
+++ 2022/Day12Part1.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 class Day12Part1
 {
     internal static async Task<string> Solution()
@@ -5,17 +7,66 @@ class Day12Part1
         var lines = await File.ReadAllLinesAsync("Day12Input.txt");
         var result = 0;
         var grid = lines.Select(l => l.ToCharArray()).ToArray();
-        var paths = new List<List<char>>();
-        var start = (0, 0);
 
+        var start = new Point();
+        var paths = Move(grid, start);
+
+        result = paths.Min(p => p.Count());
         return result.ToString();
     }
 
-    string Do(char[][] grid)
+    static List<string> Move(char[][] grid, Point current)
     {
-        foreach (var direction in directions)
+        var currentHeight = GetHeight(grid, current);
+        if (currentHeight == 'E')
         {
-            return "chosendirection" + Do(grid);
+            return new List<string> { "E" };
         }
+
+        var paths = new List<string>();
+        var neighbors = new[] {
+            new Point(current.X + 1, current.Y),
+            new Point(current.X - 1, current.Y),
+            new Point(current.X, current.Y + 1),
+            new Point(current.X, current.Y -1),
+             };
+        foreach (var neighbor in neighbors.Where(p => Valid(p, grid.Count())))
+        {
+            paths.AddRange(GetPaths(grid, currentHeight, neighbor));
+        }
+        return paths;
+    }
+
+    private static bool Valid(Point p, int size)
+    {
+        return (p.X >= 0 && p.Y >= 0) && ((p.X < size && p.Y < size));
+    }
+
+    private static IEnumerable<string> GetPaths(char[][] grid, char currentHeight, Point neighbor)
+    {
+        var neighborHeight = GetHeight(grid, neighbor);
+        if (IsOneHigher(currentHeight, neighborHeight))
+        {
+            return Move(grid, neighbor).Select(h => currentHeight.ToString() + h);
+        }
+        return new string[0];
+    }
+
+    private static char GetHeight(char[][] grid, Point p)
+    {
+        if (grid[p.X][p.Y] == 'S')
+        {
+            return 'a';
+        }
+        if (grid[p.X][p.Y] == 'E')
+        {
+            return 'z';
+        }
+        return grid[p.X][p.Y];
+    }
+
+    private static bool IsOneHigher(char currentHeight, char neighborHeight)
+    {
+        return neighborHeight == currentHeight || neighborHeight == currentHeight + 1;
     }
 }
