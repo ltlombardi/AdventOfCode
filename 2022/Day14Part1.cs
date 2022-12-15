4ef9fb09a239a2248544971949bc180ac785e51c
Leonardo T. Lombardi <lombardi.leo@gmail.com>
2022-12-15 17:03:23 -0300

    Day 14 part 1

--- 2022/Day14Part1.cs
+++ 2022/Day14Part1.cs
@@ -11,20 +11,61 @@ class Day14Part1
         var yMax = paths.SelectMany(p => p.Select(p => p.Y)).Max();
         var xMin = paths.SelectMany(p => p.Select(p => p.X)).Min();
         var xMax = paths.SelectMany(p => p.Select(p => p.X)).Max();
-        paths = paths.Select(pa => pa.Select(po => new Point(po.X -= xMin, po.Y)).ToList()).ToList(); // make point x axis zero based to match grid
+        // make points x axis zero based to match grid.
+        paths = paths.Select(pa => pa.Select(po => new Point(po.X -= xMin, po.Y)).ToList()).ToList();
+        int xLength = xMax - xMin + 1;
+        int yLength = yMax + 1;
+        var grid = new char[xLength, yLength];
+        var sandOrigin = new Point(500 - xMin, 0);
 
-        int xSize = xMax - xMin + 1;
-        int ySize = yMax + 1;
-        var grid = new char[xSize, ySize];
-        for (int i = 0; i < grid.GetLength(0); i++)
+        AddAir(grid);
+        grid[sandOrigin.X, 0] = '+'; // add origin of sand
+        AddRocks(paths, grid);
+
+        var counter = -1;
+        bool isFlowingToAbyss = false;
+        while (!isFlowingToAbyss)
         {
-            for (int j = 0; j < grid.GetLength(1); j++)
+            var x = sandOrigin.X;
+            for (int y = 0; y < grid.GetLength(1); y++)
             {
-                grid[i, j] = '.';
+                var nextY = y + 1;
+                if (x + 1 >= grid.GetLength(0) || x - 1 < 0 || nextY >= grid.GetLength(1))
+                {
+                    isFlowingToAbyss = true;
+                    break;
+                }
+                if (grid[x, nextY] == '.') { continue; }
+                else if (grid[x, nextY] == 'O' || grid[x, nextY] == '#')
+                {
+                    if (grid[x - 1, nextY] == '.')
+                    {
+                        x -= 1;
+                    }
+                    else if (grid[x + 1, nextY] == '.')
+                    {
+                        x += 1;
+                    }
+                    else
+                    {
+                        if (y != 0)
+                        {
+                            grid[x, y] = 'O';
+                        }
+                        break;
+                    }
+                }
             }
+            counter++;
         }
-        grid[500 - xMin, 0] = '+';
 
+        Print(grid, xMin, xMax);
+        result = counter;
+        return result.ToString();
+    }
+
+    private static void AddRocks(List<List<Point>> paths, char[,] grid)
+    {
         foreach (var points in paths)
         {
             if (points.Count() == 1)
@@ -34,16 +75,16 @@ class Day14Part1
             }
             for (int i = 1; i < points.Count(); i++)
             {
-                var xRange = points[i].X - points[i - 1].X;
-                var yRange = points[i].Y - points[i - 1].Y;
-                var distance = xRange + yRange;
-                var length = Math.Abs(distance);
-                var direction = distance / length;
+                var xLineLength = points[i].X - points[i - 1].X;
+                var yLineLength = points[i].Y - points[i - 1].Y;
+                var lineLength = xLineLength + yLineLength; // one of the axis is always zero
+                var absLineLength = Math.Abs(lineLength);
+                var direction = lineLength / absLineLength;
 
-                for (int j = 0; j < length; j++)
+                for (int j = 0; j <= absLineLength; j++)
                 {
                     var k = j * direction;
-                    if (xRange != 0)
+                    if (xLineLength != 0)
                     {
                         grid[points[i - 1].X + k, points[i - 1].Y] = '#';
                     }
@@ -54,8 +95,17 @@ class Day14Part1
                 }
             }
         }
-        Print(grid, xMin, xMax);
-        return result.ToString();
+    }
+
+    private static void AddAir(char[,] grid)
+    {
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                grid[i, j] = '.';
+            }
+        }
     }
 
     private static void Print(char[,] grid, int minX, int maxX)
