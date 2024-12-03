bd25b056d947da6a9f05865d91579950e4e5e565
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-03 10:13:33 -0300

    Day solution

--- 2024/Day01/Part1.cs
+++ 2024/Day01/Part1.cs
@@ -3,8 +3,23 @@ internal class Part1
 {
     internal static async Task<string> Run()
     {
-        var lines = await File.ReadAllLinesAsync(@"../../../Day01/ExampleInput.txt");
-        return "";
+        var lines = File.ReadLines(@"../../../Day01/Input.txt");
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        foreach (var line in lines)
+        {
+            var ints = line.Split("   ").Select(int.Parse).ToArray();
+            list1.Add(ints[0]);
+            list2.Add(ints[1]);
+            list1.Sort();
+            list2.Sort();
+        }
+        var sum = Enumerable.Zip(list1, list2)
+            .Select(p => Math.Abs(p.First - p.Second))
+            .Sum();
+
+
+        return sum.ToString();
     }
 }
 
--- 2024/Day01/Part2.cs
+++ 2024/Day01/Part2.cs
@@ -3,8 +3,34 @@ internal class Part2
 {
     internal static async Task<string> Run()
     {
-        var lines = await File.ReadAllLinesAsync(@"../../../Day01/ExampleInput.txt");
-        return "";
+        var lines = File.ReadLines(@"../../../Day01/Input.txt");
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        foreach (var line in lines)
+        {
+            var ints = line.Split("   ").Select(int.Parse).ToArray();
+            list1.Add(ints[0]);
+            list2.Add(ints[1]);
+            list1.Sort();
+            list2.Sort();
+        }
+        var leftSet = list1.ToHashSet();
+        var sum = 0;
+        foreach (var uniqueLeft in leftSet)
+        {
+            var count = 0;
+            foreach (var right in list2)
+            {
+                if (uniqueLeft == right)
+                {
+                    count++;
+                }
+            }
+            sum += uniqueLeft * count;
+        }
+
+
+        return sum.ToString();
     }
 }
 
