3172a47c644d8276502d8713bfe9580ba3d9339c
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-03 14:56:32 -0300

    Part 1

--- 2024/Day02/Part1.cs
+++ 2024/Day02/Part1.cs
@@ -1,10 +1,28 @@
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.ConstrainedExecution;
+
 namespace AdventOfCode2024.Day02;
 internal class Part1
 {
     internal static async Task<string> Run()
     {
         var lines = await File.ReadAllLinesAsync(@"../../../Day02/Input.txt");
-        return "";
+        var numberOfSafeReports = 0;
+        foreach (var line in lines)
+        {
+            var levels = line.Split(' ').Select(int.Parse).ToList();
+            var isSafe = true;
+            var initialDirection = levels[1] - levels[0] > 0;
+            for (var i = 0; i < levels.Count - 1; i++)
+            {
+                var cur = levels[i];
+                var next = levels[i + 1];
+                var direction = next - cur > 0;
+                isSafe &= Math.Abs(next - cur) >= 1 && Math.Abs(next - cur) <= 3 && (direction == initialDirection);
+            }
+            if (isSafe) numberOfSafeReports++;
+        }
+        return numberOfSafeReports.ToString();
     }
 }
 
--- 2024/Day02/Part2.cs
+++ 2024/Day02/Part2.cs
@@ -4,7 +4,42 @@ internal class Part2
     internal static async Task<string> Run()
     {
         var lines = await File.ReadAllLinesAsync(@"../../../Day02/Input.txt");
-        return "";
+        var numberOfSafeReports = 0;
+        foreach (var line in lines)
+        {
+            var levels = line.Split(' ').Select(int.Parse).ToList();
+            var isSafe = true;
+            var marginUsed = false;
+            var levelToSkip = -1;
+            var initialDirection = levels[1] - levels[0] > 0;
+            for (var i = 0; i < levels.Count - 1; i++)
+            {
+                var cur = levels[i];
+                if (i == levelToSkip)
+                {
+                    cur = levels[i - 1];
+                }
+                var next = levels[i + 1];
+                var res = IsSafe(initialDirection, cur, next);
+                if (!res && !marginUsed)
+                {
+                    levelToSkip = i + 1;
+                    if (levelToSkip == levels.Count - 1) continue;
+                    res = IsSafe(initialDirection, cur, levels[i + 2]);
+                    marginUsed = true;
+                }
+                isSafe &= res;
+            }
+            if (isSafe) numberOfSafeReports++;
+        }
+        return numberOfSafeReports.ToString();
+    }
+
+    private static bool IsSafe(bool initialDirection, int cur, int next)
+    {
+        var direction = next - cur > 0;
+
+        return Math.Abs(next - cur) >= 1 && Math.Abs(next - cur) <= 3 && (direction == initialDirection);
     }
 }
 
