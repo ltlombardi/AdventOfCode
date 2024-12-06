38ba1b8dec441f95389f41be2659482e45e28122
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-06 16:06:30 -0300

    Day 4 part 1

--- 2024/Day04/Part1.cs
+++ 2024/Day04/Part1.cs
@@ -6,14 +6,46 @@ internal class Part1
 {
     internal static async Task<string> Run()
     {
-        var lines = await File.ReadAllLinesAsync(@"../../../Day04/TestInput.txt");
-        var pattern = @"XMAS|SAMX";
+        var lines = await File.ReadAllLinesAsync(@"../../../Day04/Input.txt");
+        var pattern = @"(?=(XMAS|SAMX))";
         var sum = 0;
         sum += Count(lines, pattern);
         sum += Count(Transpose(lines), pattern);
         sum += Count(GenerateDiagonalTopToBottom(lines), pattern);
-        //sum += Count(GenerateBackSlashDiag(lines), pattern);
+        sum += Count(GenerateDiagonalBottomToTop(lines), pattern);
         return sum.ToString();
+    }
+
+    private static IEnumerable<string> GenerateDiagonalBottomToTop(string[] lines)
+    {
+        int rows = lines.Length;
+        int cols = lines[0].Length;
+        List<string> diagonals = new List<string>();
+        var sb = new StringBuilder();
+
+        // image each column being tilted to become a diagonal. Covers the upper triangle + midle diagonal
+        for (int col = cols - 1; col >= 0; col--)
+        {
+            for (int row = 0, c = col; row < rows && c >= 0; row++, c--)
+            {
+                sb.Append(lines[row][c]);
+            }
+            diagonals.Add(sb.ToString());
+            sb.Clear();
+        }
+
+        // image each row being tilted to become a diagonal, starting at second row. Covers bottom triangle
+        for (int row = 1; row < rows; row++)
+        {
+            for (int r = row, col = cols - 1; r < rows && col >= 0; r++, col--)
+            {
+                sb.Append(lines[r][col]);
+            }
+            diagonals.Add(sb.ToString());
+            sb.Clear();
+        }
+
+        return diagonals;
     }
 
     private static IEnumerable<string> GenerateDiagonalTopToBottom(string[] lines)
@@ -54,13 +86,14 @@ internal class Part1
     {
         var newLines = new List<string>();
         var sb = new StringBuilder();
-        for (var i = 0; i < lines[0].Length; i++)
+        for (var col = 0; col < lines[0].Length; col++)
         {
             foreach (var line in lines)
             {
-                sb.Append(line[i]);
+                sb.Append(line[col]);
             }
             newLines.Add(sb.ToString());
+            sb.Clear();
         }
         return newLines;
     }
@@ -70,9 +103,8 @@ internal class Part1
         var sum = 0;
         foreach (var line in lines)
         {
-            sum = Regex.Count(line, pattern);
+            sum += Regex.Count(line, pattern);
         }
-
         return sum;
     }
 }
