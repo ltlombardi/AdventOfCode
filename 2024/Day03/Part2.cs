079c8bf9cd22a4aa350886bf2efbd40bc05b5272
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-05 21:27:32 -0300

    Improvement day3

--- 2024/Day03/Part2.cs
+++ 2024/Day03/Part2.cs
@@ -7,6 +7,25 @@ internal class Part2
     {
         var line = await File.ReadAllTextAsync(@"../../../Day03/Input.txt");
         var pattern = @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)";
+        var matches = Regex.Matches(line, pattern);
+        var sum = 0;
+        var isEnabled = true;
+        foreach (Match match in matches)
+        {
+            isEnabled = !match.Value.Equals("don't()") || match.Value.Equals("do()");
+
+            if (isEnabled && match.Value.StartsWith("mul"))
+            {
+                sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+            }
+        }
+        return sum.ToString();
+    }
+
+    internal static async Task<string> RunOld()
+    {
+        var line = await File.ReadAllTextAsync(@"../../../Day03/Input.txt");
+        var pattern = @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)";
         var match = Regex.Match(line, pattern);
         var sum = 0;
         var isEnabled = true;
