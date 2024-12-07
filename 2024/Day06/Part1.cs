9ad89702e3ffa6e412d5b677a507e52044a0d4c6
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-07 20:07:13 -0300

    Day 6 part 1

--- 2024/Day06/Part1.cs
+++ 2024/Day06/Part1.cs
@@ -4,7 +4,26 @@ internal class Part1
     internal static async Task<dynamic> Run()
     {
         var lines = await File.ReadAllLinesAsync(@"../../../Day06/Input.txt");
-        return "";
+        var rules = lines.TakeWhile(l => !string.IsNullOrEmpty(l))
+                         .Select(l => l.Split('|').Select(int.Parse).ToList())
+                         .ToList();
+        var updates = lines.SkipWhile(l => !string.IsNullOrEmpty(l)).Skip(1).Select(l => l.Split(',').Select(int.Parse).ToList()).ToList();
+        var sum = 0;
+        foreach (var update in updates)
+        {
+            var illegal = false;
+            for (var p = 0; p < update.Count; p++)
+            {
+                var pagesBefore = update.Take(p).ToList();
+                var page = update[p];
+                if (rules.Any(r => r[0] == page && pagesBefore.Contains(r[1])))
+                {
+                    illegal = true;
+                }
+            }
+            if (!illegal) sum += update[(update.Count - 1) / 2];
+        }
+        return sum;
     }
 }
 
