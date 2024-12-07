0a6d87e3c38ec3ba5e0ad05468d8a91bb3ec2dfb
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-07 20:07:46 -0300

    Update gitignore. Day 5 part 1 and 2.

--- 2024/Day05/Part1.cs
+++ 2024/Day05/Part1.cs
@@ -4,7 +4,27 @@ internal class Part1
     internal static async Task<dynamic> Run()
     {
         var lines = await File.ReadAllLinesAsync(@"../../../Day05/Input.txt");
-        return "";
+        var rules = lines.TakeWhile(l => !string.IsNullOrEmpty(l))
+                         .Select(l => l.Split('|').Select(int.Parse).ToList())
+                         .Select(i => (pageBefore: i[0], pageAfter: i[1]))
+                         .ToList();
+        var updates = lines.SkipWhile(l => !string.IsNullOrEmpty(l)).Skip(1).Select(l => l.Split(',').Select(int.Parse).ToList()).ToList();
+        var sum = 0;
+        foreach (var update in updates)
+        {
+            var illegal = false;
+            for (var p = 0; p < update.Count; p++)
+            {
+                var pagesBefore = update.Take(p).ToList();
+                var page = update[p];
+                if (rules.Any(r => r.pageBefore == page && pagesBefore.Contains(r.pageAfter)))
+                {
+                    illegal = true;
+                }
+            }
+            if (!illegal) sum += update[(update.Count - 1) / 2];
+        }
+        return sum;
     }
 }
 
--- 2024/Day05/Part2.cs
+++ 2024/Day05/Part2.cs
@@ -4,7 +4,34 @@ internal class Part2
     internal static async Task<dynamic> Run()
     {
         var lines = await File.ReadAllLinesAsync(@"../../../Day05/Input.txt");
-        return "";
+        var pageOrderingRules = lines.TakeWhile(l => !string.IsNullOrEmpty(l))
+                         .Select(l => l.Split('|').Select(int.Parse).ToList())
+                         .Select(i => (i[0], i[1]))
+                         .ToList();
+        var updates = lines.SkipWhile(l => !string.IsNullOrEmpty(l)).Skip(1).Select(l => l.Split(',').Select(int.Parse).ToList()).ToList();
+        var sum = 0;
+        foreach (var pagesToProduce in updates)
+        {
+            var illegal = false;
+            for (var p = 0; p < pagesToProduce.Count; p++)
+            {
+                foreach (var rule in pageOrderingRules)
+                {
+                    var pagesBeforeCurrent = pagesToProduce.Take(p).ToList();
+                    var pageUnderAnalysis = pagesToProduce[p];
+                    var invalidPageIndex = pagesBeforeCurrent.IndexOf(rule.Item2);
+                    if (rule.Item1 == pageUnderAnalysis && invalidPageIndex != -1)
+                    {
+                        pagesToProduce.Insert(p + 1, rule.Item2);
+                        pagesToProduce.RemoveAt(invalidPageIndex);
+                        p--;
+                        illegal = true;
+                    }
+                }
+            }
+            if (illegal) sum += pagesToProduce[(pagesToProduce.Count - 1) / 2];
+        }
+        return sum;
     }
 }
 
