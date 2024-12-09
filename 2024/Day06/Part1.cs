07c62d8b31d0d3630603841712f874f0b17014d0
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-08 21:32:24 -0300

    Day 6 part 1 and part 2 wip

--- 2024/Day06/Part1.cs
+++ 2024/Day06/Part1.cs
@@ -3,8 +3,106 @@ internal class Part1
 {
     internal static async Task<dynamic> Run()
     {
-        var lines = await File.ReadAllLinesAsync(@"../../../Day06");
-        return "";
+        var rawLines = await File.ReadAllLinesAsync(@"../../../Day06/Input.txt");
+        var lines = rawLines.Select(x => x.ToCharArray()).ToArray();
+        var pos = (row: -1, col: -1);
+        for (var row = 0; row < lines.Length; row++)
+        {
+            for (var col = 0; col < lines[0].Length; col++)
+            {
+                if (lines[row][col] == '^')
+                {
+                    pos = (row, col);
+                }
+            }
+        }
+        var direction = Direction.Up;
+        var visited = 0;
+        while (pos.row < lines.Length && pos.row >= 0 && pos.col >= 0 && pos.col < lines[0].Length)
+        {
+            if (direction == Direction.Up)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] != 'X')
+                    {
+                        lines[pos.row][pos.col] = 'X';
+                        visited++;
+                    }
+                    pos.row--;
+                }
+                else
+                {
+                    pos.row++;
+                    direction = Direction.Right;
+                }
+            }
+            else if (direction == Direction.Right)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] != 'X')
+                    {
+                        lines[pos.row][pos.col] = 'X';
+                        visited++;
+                    }
+                    pos.col++;
+                }
+                else
+                {
+                    pos.col--;
+                    direction = Direction.Down;
+                }
+            }
+            else if (direction == Direction.Down)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] != 'X')
+                    {
+                        lines[pos.row][pos.col] = 'X';
+                        visited++;
+                    }
+                    pos.row++;
+                }
+                else
+                {
+                    pos.row--;
+                    direction = Direction.Left;
+                }
+            }
+            else if (direction == Direction.Left)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] != 'X')
+                    {
+                        lines[pos.row][pos.col] = 'X';
+                        visited++;
+                    }
+                    pos.col--;
+                }
+                else
+                {
+                    pos.col++;
+                    direction = Direction.Up;
+                }
+            }
+        }
+        foreach (var line in lines)
+        {
+            foreach (var letter in line)
+            {
+                Console.Write(letter);
+            }
+            Console.WriteLine();
+        }
+        return visited;
+    }
+
+    enum Direction
+    {
+        Left, Right, Up, Down
     }
 }
 
--- 2024/Day06/Part2.cs
+++ 2024/Day06/Part2.cs
@@ -1,10 +1,154 @@
+using System;
+using System.Diagnostics.Metrics;
+
 namespace AdventOfCode2024.Day06;
 internal class Part2
 {
     internal static async Task<dynamic> Run()
     {
-        var lines = await File.ReadAllLinesAsync(@"../../../Day06/Input.txt");
-        return "";
+        var rawLines = await File.ReadAllLinesAsync(@"../../../Day06/ExampleInput.txt");
+        var lines = rawLines.Select(x => x.ToCharArray()).ToArray();
+        var pos = new Position(-1, -1);
+        for (var row = 0; row < lines.Length; row++)
+        {
+            for (var col = 0; col < lines[0].Length; col++)
+            {
+                if (lines[row][col] == '^')
+                {
+                    pos = new Position(row, col);
+                }
+            }
+        }
+        var direction = Direction.Up;
+        var options = 0;
+
+        while (InsideMap(lines, pos))
+        {
+            if (direction == Direction.Up)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] == Direction.Right)
+                    {
+                        options++;
+                        lines[pos.row - 1][pos.col] = 'O';
+                        Print(lines);
+                    }
+
+                    lines[pos.row][pos.col] = Direction.Up;
+                    pos.row--;
+                }
+                else
+                {
+                    pos.row++;
+                    lines[pos.row][pos.col] = Direction.Right;
+                    direction = Direction.Right;
+                }
+            }
+            else if (direction == Direction.Right)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] == Direction.Down)
+                    {
+                        options++;
+                        lines[pos.row][pos.col + 1] = 'O';
+                        Print(lines);
+                    }
+
+                    lines[pos.row][pos.col] = Direction.Right;
+                    pos.col++;
+                }
+                else
+                {
+                    pos.col--;
+                    lines[pos.row][pos.col] = Direction.Down;
+                    direction = Direction.Down;
+                }
+            }
+            else if (direction == Direction.Down)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] == Direction.Left)
+                    {
+                        options++;
+                        lines[pos.row + 1][pos.col] = 'O';
+                        Print(lines);
+                    }
+
+                    lines[pos.row][pos.col] = Direction.Down;
+                    pos.row++;
+                }
+                else
+                {
+                    pos.row--;
+                    lines[pos.row][pos.col] = Direction.Left;
+                    direction = Direction.Left;
+                }
+            }
+            else if (direction == Direction.Left)
+            {
+                if (lines[pos.row][pos.col] != '#')
+                {
+                    if (lines[pos.row][pos.col] == Direction.Up)
+                    {
+                        options++;
+                        lines[pos.row][pos.col - 1] = 'O';
+                        Print(lines);
+                    }
+                    lines[pos.row][pos.col] = Direction.Left;
+                    pos.col--;
+                }
+                else
+                {
+                    pos.col++;
+                    lines[pos.row][pos.col] = Direction.Up;
+                    direction = Direction.Up;
+                }
+            }
+            Print(lines);
+        }
+
+        return options;
+    }
+
+    private static void Print(char[][] lines)
+    {
+        foreach (var line in lines)
+        {
+            foreach (var letter in line)
+            {
+                Console.Write(letter);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("_____________________");
+    }
+
+    private static bool InsideMap(char[][] lines, Position pos)
+    {
+        return pos.row < lines.Length && pos.row >= 0 && pos.col >= 0 && pos.col < lines[0].Length;
+    }
+
+    static class Direction
+    {
+        public static char Left = '<';
+        public static char Right = '>';
+        public static char Up = '^';
+        public static char Down = 'v';
     }
 }
 
+internal record struct Position(int row, int col)
+{
+    public static implicit operator (int row, int col)(Position value)
+    {
+        return (value.row, value.col);
+    }
+
+    public static implicit operator Position((int row, int col) value)
+    {
+        return new Position(value.row, value.col);
+    }
+}
