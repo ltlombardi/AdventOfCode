c035b2a60d55e030f9d7cbac54fe73b1ebf73a13
Leonardo Trevisan Lombardi <lombardi.leo@gmail.com>
2024-12-09 15:51:38 -0300

    WIP

--- 2024/Day06/Part2.cs
+++ 2024/Day06/Part2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Immutable;
 using System.Diagnostics.Metrics;
 
 namespace AdventOfCode2024.Day06;
@@ -20,105 +21,110 @@ internal class Part2
             }
         }
         var direction = Direction.Up;
-        var options = 0;
+        var options = new HashSet<Position>();
 
+        var optionsTask = new List<Task>();
         while (InsideMap(lines, pos))
         {
-            if (direction == Direction.Up)
+            if (lines[pos.row][pos.col] == '#')
             {
-                if (lines[pos.row][pos.col] != '#')
-                {
-                    if (lines[pos.row][pos.col] == Direction.Right)
-                    {
-                        options++;
-                        lines[pos.row - 1][pos.col] = 'O';
-                        Print(lines);
-                    }
+                GoBackAndChangeDirection(ref pos, ref direction);
+            }
+            else
+            {
+                //optionsTask.Add(Task.Run(() => CheckPossibleLoop(lines, pos, direction, options)));
+                CheckPossibleLoop(lines, pos, direction, options);
 
-                    lines[pos.row][pos.col] = Direction.Up;
-                    pos.row--;
-                }
-                else
-                {
-                    pos.row++;
-                    lines[pos.row][pos.col] = Direction.Right;
-                    direction = Direction.Right;
-                }
+                pos = Advance(pos, direction);
             }
-            else if (direction == Direction.Right)
-            {
-                if (lines[pos.row][pos.col] != '#')
-                {
-                    if (lines[pos.row][pos.col] == Direction.Down)
-                    {
-                        options++;
-                        lines[pos.row][pos.col + 1] = 'O';
-                        Print(lines);
-                    }
-
-                    lines[pos.row][pos.col] = Direction.Right;
-                    pos.col++;
-                }
-                else
-                {
-                    pos.col--;
-                    lines[pos.row][pos.col] = Direction.Down;
-                    direction = Direction.Down;
-                }
-            }
-            else if (direction == Direction.Down)
-            {
-                if (lines[pos.row][pos.col] != '#')
-                {
-                    if (lines[pos.row][pos.col] == Direction.Left)
-                    {
-                        options++;
-                        lines[pos.row + 1][pos.col] = 'O';
-                        Print(lines);
-                    }
-
-                    lines[pos.row][pos.col] = Direction.Down;
-                    pos.row++;
-                }
-                else
-                {
-                    pos.row--;
-                    lines[pos.row][pos.col] = Direction.Left;
-                    direction = Direction.Left;
-                }
-            }
-            else if (direction == Direction.Left)
-            {
-                if (lines[pos.row][pos.col] != '#')
-                {
-                    if (lines[pos.row][pos.col] == Direction.Up)
-                    {
-                        options++;
-                        lines[pos.row][pos.col - 1] = 'O';
-                        Print(lines);
-                    }
-                    lines[pos.row][pos.col] = Direction.Left;
-                    pos.col--;
-                }
-                else
-                {
-                    pos.col++;
-                    lines[pos.row][pos.col] = Direction.Up;
-                    direction = Direction.Up;
-                }
-            }
-            Print(lines);
         }
-
-        return options;
+        //await Task.WhenAll(optionsTask);
+        //Print(lines, pos);
+        return options.Count;
     }
 
-    private static void Print(char[][] lines)
+    private static void GoBackAndChangeDirection(ref Position pos, ref char direction)
     {
-        foreach (var line in lines)
+        if (direction == Direction.Up)
         {
-            foreach (var letter in line)
+            pos.row++;
+            direction = Direction.Right;
+        }
+        else if (direction == Direction.Right)
+        {
+            pos.col--;
+            direction = Direction.Down;
+        }
+        else if (direction == Direction.Down)
+        {
+            pos.row--;
+            direction = Direction.Left;
+
+        }
+        else if (direction == Direction.Left)
+        {
+            pos.col++;
+            direction = Direction.Up;
+        }
+    }
+
+    private static Position Advance(Position pos, char direction)
+    {
+        if (direction == Direction.Up)
+        {
+            pos.row--;
+        }
+        else if (direction == Direction.Right)
+        {
+            pos.col++;
+        }
+        else if (direction == Direction.Down)
+        {
+            pos.row++;
+        }
+        else if (direction == Direction.Left)
+        {
+            pos.col--;
+        }
+
+        return pos;
+    }
+
+    private static void CheckPossibleLoop(char[][] lines, Position pos, char direction, HashSet<Position> options)
+    {
+        var fakeObstacle = pos;
+        var visited = new HashSet<(Position, char)>();
+        while (InsideMap(lines, pos))
+        {
+            if (visited.Contains((pos, direction)))
             {
+                options.Add(fakeObstacle);
+                return;
+            }
+            if (lines[pos.row][pos.col] == '#' || pos == fakeObstacle)
+            {
+                GoBackAndChangeDirection(ref pos, ref direction);
+            }
+            else
+            {
+                visited.Add((pos, direction));
+                pos = Advance(pos, direction);
+            }
+        }
+    }
+
+    private static void Print(char[][] lines, Position position)
+    {
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            for (var j = 0; j < line.Length; j++)
+            {
+                var letter = line[j];
+                if (new Position(i, j) == position)
+                {
+                    letter = 'O';
+                }
                 Console.Write(letter);
             }
             Console.WriteLine();
